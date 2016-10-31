cddeb41890b29ab5759cd670696524f1686e9ca4
Sergii Kliuchnyk <redexp@ukr.net>
2016-10-31 07:13:01 +0200

    Update script.js
    с `closest` стало немного понятнее, `return false` делать не было ни какого смысла

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 jQuery(function ($){
     $('button.up').click(function(event){
-        var parentElem = $(this).parent().parent();
+        var parentElem = $(this).closest('li');
         parentElem.insertBefore(parentElem.prev());
-        return false
     });
     $('button.down').click(function(event){
-        var parentElem = $(this).parent().parent();
+        var parentElem = $(this).closest('li');
         parentElem.insertAfter(parentElem.next());
-        return false
     });
 } );
